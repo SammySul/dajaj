a90656a923e094f22db34eab0aa0cbf95e96a88e
Sami Suleiman <sulsami@protonmail.com>
2024-11-03 18:23:16 +0100

    server(wip): add initial caching

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { LeaderboardModule } from './leaderboard/leaderboard.module';
 import { HttpModule } from '@nestjs/axios';
+import { CacheModule } from '@nestjs/cache-manager';
 
 @Module({
   imports: [
+    CacheModule.register({ isGlobal: true }),
     LeaderboardModule,
     ConfigModule.forRoot({ isGlobal: true }),
     HttpModule.register({ global: true }),
--- server/src/leaderboard/leaderboard.service.ts
+++ server/src/leaderboard/leaderboard.service.ts
@@ -1,8 +1,10 @@
 import { HttpService } from '@nestjs/axios';
-import { Injectable, Logger } from '@nestjs/common';
+import { CACHE_MANAGER } from '@nestjs/cache-manager';
+import { Inject, Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { AxiosError } from 'axios';
-import { catchError, map, Observable, of } from 'rxjs';
+import { Cache } from 'cache-manager';
+import { catchError, from, map, Observable, of, switchMap, tap } from 'rxjs';
 import { PUBG_API_URL } from './leaderboard.consts';
 import { MatchResponseDto, PlayerResponseDto } from './leaderboard.model';
 
@@ -14,26 +16,48 @@ export class LeaderboardService {
   constructor(
     private readonly httpService: HttpService,
     private readonly configService: ConfigService,
+    @Inject(CACHE_MANAGER) private cacheManager: Cache,
   ) {}
 
   private fetchPlayers$(playerNames: string[]): Observable<PlayerResponseDto> {
     const url =
       `${PUBG_API_URL}/players?filter[playerNames]=` + playerNames.join(',');
 
-    return this.httpService
-      .get<PlayerResponseDto>(url, {
-        headers: {
-          Accept: 'application/vnd.api+json',
-          Authorization: `Bearer ${this.apiKey}`,
-        },
-      })
-      .pipe(
-        map((response) => response.data),
-        catchError((error: AxiosError) => {
-          Logger.error(error.message, error.stack, LeaderboardService.name);
-          return of(null);
-        }),
-      );
+    return from(this.cacheManager.get<PlayerResponseDto>('players')).pipe(
+      switchMap((cachedPlayers) => {
+        if (!!cachedPlayers)
+          return of(cachedPlayers).pipe(
+            tap(() => console.log('Cache hit, fetching from cache')),
+          );
+        else {
+          {
+            return this.httpService
+              .get<PlayerResponseDto>(url, {
+                headers: {
+                  Accept: 'application/vnd.api+json',
+                  Authorization: `Bearer ${this.apiKey}`,
+                },
+              })
+              .pipe(
+                map((response) => response.data),
+                tap(() => console.log('Cache miss, fetching from API')),
+                tap(
+                  async (data) =>
+                    await this.cacheManager.set('players', data, 5 * 60 * 1000),
+                ),
+                catchError((error: AxiosError) => {
+                  Logger.error(
+                    error.message,
+                    error.stack,
+                    LeaderboardService.name,
+                  );
+                  return of(null);
+                }),
+              );
+          }
+        }
+      }),
+    );
   }
 
   private fetchMatch$(matchId: string): Observable<MatchResponseDto> {
