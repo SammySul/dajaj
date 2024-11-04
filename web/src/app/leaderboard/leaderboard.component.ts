4d179b0e8dafe790f5a6cb4d5b568b90dc6e2747
Sami Suleiman <sulsami@protonmail.com>
2024-11-04 14:56:39 +0100

    web: initial config

---
+++ web/src/app/leaderboard/leaderboard.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  template: ` <p>leaderboard works!</p> `,
+  styles: ``,
+  imports: [],
+  selector: "app-leaderboard",
+  standalone: true,
+})
+export class LeaderboardComponent {}
