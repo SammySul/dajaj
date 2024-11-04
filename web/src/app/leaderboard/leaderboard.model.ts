759a2e5ebc56aa28f4a89cc7a1a8e236840aea38
Sami Suleiman <sulsami@protonmail.com>
2024-11-04 15:09:36 +0100

    web: add leaderboard model

---
+++ web/src/app/leaderboard/leaderboard.model.ts
@@ -0,0 +1,22 @@
+export interface LeaderboardDto {
+  playerName: string;
+  stats: LeaderboardStats;
+}
+
+export interface LeaderboardStats {
+  assists: number;
+  damage: number;
+  headshotKills: number;
+  heals: number;
+  kills: number;
+  longestKill: number;
+  revives: number;
+  rideDistance: number;
+  roadKills: number;
+  swimDistance: number;
+  teamKills: number;
+  timeSurvived: number;
+  vehicleDestroys: number;
+  walkDistance: number;
+  weaponsAcquired: number;
+}
