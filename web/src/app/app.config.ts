cb29ed8a5e5ee18fb9f6d6d16b17232ba1aa4353
Sami Suleiman <sulsami@protonmail.com>
2024-11-15 18:53:36 +0100

    web: add i18n

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,6 +1,7 @@
 import {
   ApplicationConfig,
   provideExperimentalZonelessChangeDetection,
+  isDevMode,
 } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
@@ -8,6 +9,8 @@ import { provideHttpClient } from '@angular/common/http';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { routes } from './app.routes';
 import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
+import { TranslocoHttpLoader } from './transloco-loader';
+import { provideTransloco } from '@jsverse/transloco';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -16,5 +19,25 @@ export const appConfig: ApplicationConfig = {
     provideExperimentalZonelessChangeDetection(),
     provideAnimationsAsync(),
     provideCharts(withDefaultRegisterables()),
+    provideHttpClient(),
+    provideTransloco({
+      config: {
+        availableLangs: [
+          {
+            label: 'Arabic',
+            id: 'ar',
+          },
+          {
+            label: 'English',
+            id: 'en',
+          },
+        ],
+        defaultLang: 'ar',
+        // Remove this option if your application doesn't support changing language in runtime.
+        reRenderOnLangChange: true,
+        prodMode: !isDevMode(),
+      },
+      loader: TranslocoHttpLoader,
+    }),
   ],
 };
